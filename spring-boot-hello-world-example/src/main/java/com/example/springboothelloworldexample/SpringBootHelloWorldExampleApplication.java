e26fbfae67cbaf703fbc622f04f6ab238344013d
guehara-code <gil_uehara@yahoo.com.br>
2024-04-28 08:33:44 -0300

    more studies

--- spring-boot-hello-world-example/src/main/java/com/example/springboothelloworldexample/SpringBootHelloWorldExampleApplication.java
+++ spring-boot-hello-world-example/src/main/java/com/example/springboothelloworldexample/SpringBootHelloWorldExampleApplication.java
@@ -1,13 +1,21 @@
 package com.example.springboothelloworldexample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootHelloWorldExampleApplication {
+public class SpringBootHelloWorldExampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(" *** Show arguments *** ");
+		Arrays.stream(args).forEach(System.out::println);
+	}
 }
