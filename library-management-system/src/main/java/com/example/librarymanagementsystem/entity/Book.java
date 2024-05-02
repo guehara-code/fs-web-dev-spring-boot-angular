e77362d18c55fdbededf871bbfed598e6bf0f6f6
guehara-code <gil_uehara@yahoo.com.br>
2024-05-02 08:49:08 -0300

    more studies

---
+++ library-management-system/src/main/java/com/example/librarymanagementsystem/entity/Book.java
@@ -0,0 +1,21 @@
+package com.example.librarymanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+}
