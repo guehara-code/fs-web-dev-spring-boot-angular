dbe01786af782ce009e04afbf9f204d7b6040df7
guehara-code <gil_uehara@yahoo.com.br>
2024-05-08 08:26:25 -0300

    more studies

--- admin/src/main/java/com/javacorner/admin/entity/Instructor.java
+++ admin/src/main/java/com/javacorner/admin/entity/Instructor.java
@@ -1,6 +1,8 @@
 package com.javacorner.admin.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Instructor {
 
@@ -8,6 +10,10 @@ public class Instructor {
     private String firstName;
     private String lastName;
     private String summary;
+
+    private Set<Course> courses = new HashSet<>();
+
+    private User user;
 
     @Override
     public boolean equals(Object o) {
@@ -20,5 +26,73 @@ public class Instructor {
     @Override
     public int hashCode() {
         return Objects.hash(instructorId, firstName, lastName, summary);
+    }
+
+    public Instructor() {
+    }
+
+    public Instructor(String firstName, String lastName, String summary, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.summary = summary;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "instructorId=" + instructorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", summary='" + summary + '\'' +
+                '}';
+    }
+
+    public Long getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(Long instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
