a2254c847ad66b0717ba623c050ffb11e9170ddf
guehara-code <gil_uehara@yahoo.com.br>
2024-05-08 09:05:25 -0300

    more studies

--- admin/src/main/java/com/javacorner/admin/entity/Role.java
+++ admin/src/main/java/com/javacorner/admin/entity/Role.java
@@ -1,11 +1,15 @@
 package com.javacorner.admin.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Role {
 
     private Long roleId;
     private String name;
+
+    private Set<User> users = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
@@ -18,5 +22,44 @@ public class Role {
     @Override
     public int hashCode() {
         return Objects.hash(roleId, name);
+    }
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 }
