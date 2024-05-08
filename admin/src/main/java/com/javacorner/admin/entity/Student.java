4dae97bfa219cf7e3b4719430d2a29fef09cdf23
guehara-code <gil_uehara@yahoo.com.br>
2024-05-08 08:29:36 -0300

    more studies

--- admin/src/main/java/com/javacorner/admin/entity/Student.java
+++ admin/src/main/java/com/javacorner/admin/entity/Student.java
@@ -1,6 +1,8 @@
 package com.javacorner.admin.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Student {
 
@@ -8,6 +10,10 @@ public class Student {
     private String firstName;
     private String lastName;
     private String level;
+
+    private Set<Course> courses = new HashSet<>();
+
+    private User user;
 
     @Override
     public boolean equals(Object o) {
@@ -20,5 +26,73 @@ public class Student {
     @Override
     public int hashCode() {
         return Objects.hash(studentId, firstName, lastName, level);
+    }
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String level, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.level = level;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", level='" + level + '\'' +
+                '}';
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
