723eed40fe5813122f2b0c34c3ae4265f6f7868f
guehara-code <gil_uehara@yahoo.com.br>
2024-05-09 08:22:39 -0300

    more studies

--- admin/src/main/java/com/javacorner/admin/entity/Instructor.java
+++ admin/src/main/java/com/javacorner/admin/entity/Instructor.java
@@ -1,18 +1,34 @@
 package com.javacorner.admin.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "instructors")
 public class Instructor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_id", nullable = false)
     private Long instructorId;
+    @Basic
+    @Column(name = "first_name", nullable = false,length = 45)
     private String firstName;
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 45)
     private String lastName;
+    @Basic
+    @Column(name = "summary", nullable = false, length = 64)
     private String summary;
 
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY)
     private Set<Course> courses = new HashSet<>();
 
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
     private User user;
 
     @Override
