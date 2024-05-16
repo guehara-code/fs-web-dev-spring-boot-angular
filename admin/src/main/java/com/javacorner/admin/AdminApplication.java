a359f63cb9d3e0953b9742643469e92c655f3814
guehara-code <gil_uehara@yahoo.com.br>
2024-05-16 08:32:34 -0300

    more studies

--- admin/src/main/java/com/javacorner/admin/AdminApplication.java
+++ admin/src/main/java/com/javacorner/admin/AdminApplication.java
@@ -1,42 +1,42 @@
 package com.javacorner.admin;
 
-import com.javacorner.admin.dao.*;
-import com.javacorner.admin.utility.OperationUtility;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+//import com.javacorner.admin.dao.*;
+//import com.javacorner.admin.utility.OperationUtility;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminApplication implements CommandLineRunner {
+public class AdminApplication {
 
-	@Autowired
-	private UserDao userDao;
-
-	@Autowired
-	private CourseDao courseDao;
-
-	@Autowired
-	private InstructorDao instructorDao;
-
-	@Autowired
-	private StudentDao studentDao;
-
-	@Autowired
-	private RoleDao roleDao;
+//	@Autowired
+//	private UserDao userDao;
+//
+//	@Autowired
+//	private CourseDao courseDao;
+//
+//	@Autowired
+//	private InstructorDao instructorDao;
+//
+//	@Autowired
+//	private StudentDao studentDao;
+//
+//	@Autowired
+//	private RoleDao roleDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-//		OperationUtility.usersOperations(userDao);
-//		OperationUtility.rolesOperations(roleDao);
-//		OperationUtility.assignRolesToUsers(userDao, roleDao);
-//		OperationUtility.instructorsOperations(userDao, instructorDao, roleDao);
-//		OperationUtility.studentsOperations(userDao, studentDao, roleDao);
-		OperationUtility.coursesOperations(courseDao, instructorDao, studentDao);
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+////		OperationUtility.usersOperations(userDao);
+////		OperationUtility.rolesOperations(roleDao);
+////		OperationUtility.assignRolesToUsers(userDao, roleDao);
+////		OperationUtility.instructorsOperations(userDao, instructorDao, roleDao);
+////		OperationUtility.studentsOperations(userDao, studentDao, roleDao);
+//		OperationUtility.coursesOperations(courseDao, instructorDao, studentDao);
+//	}
 
 }
