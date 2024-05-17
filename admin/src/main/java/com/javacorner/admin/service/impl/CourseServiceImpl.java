7fb240d996270280b5f3176b56dbef4c4f4f5db9
guehara-code <gil_uehara@yahoo.com.br>
2024-05-17 20:46:13 -0300

    more studies

---
+++ admin/src/main/java/com/javacorner/admin/service/impl/CourseServiceImpl.java
@@ -0,0 +1,79 @@
+package com.javacorner.admin.service.impl;
+
+import com.javacorner.admin.dao.InstructorDao;
+import com.javacorner.admin.dto.CourseDTO;
+import com.javacorner.admin.entity.Course;
+import com.javacorner.admin.entity.Instructor;
+import com.javacorner.admin.mapper.CourseMapper;
+import com.javacorner.admin.mapper.InstructorMapper;
+import com.javacorner.admin.service.CourseService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import com.javacorner.admin.dao.CourseDao;
+
+public class CourseServiceImpl implements CourseService {
+
+    private CourseDao courseDao;
+
+    private CourseMapper courseMapper;
+
+    private InstructorDao instructorDao;
+
+    public CourseServiceImpl(CourseDao courseDao, CourseMapper courseMapper, InstructorDao instructorDao) {
+        this.courseDao = courseDao;
+        this.courseMapper = courseMapper;
+        this.instructorDao = instructorDao;
+    }
+
+
+    @Override
+    public Course loadCourseById(Long courseId) {
+
+        return courseDao.findById(courseId).orElseThrow(() -> new EntityNotFoundException("Course with ID " + courseId + " Not Found"));
+    }
+
+    @Override
+    public CourseDTO createCourse(CourseDTO courseDTO) {
+
+        Course course = courseMapper.fromCourseDTO(courseDTO);
+        Instructor instructor = instructorDao.findById(courseDTO.getInstructor().getInstructorId()).orElseThrow(() -> new EntityNotFoundException("Instructor with ID " + courseDTO.getInstructor().getInstructorId() + " Not Found"));
+        course.setInstructor(instructor);
+        Course savedCourse = courseDao.save(course);
+        return courseMapper.fromCourse(savedCourse);
+    }
+
+    @Override
+    public CourseDTO updateCourse(CourseDTO courseDTO) {
+        return null;
+    }
+
+    @Override
+    public Page<CourseDTO> findCoursesByCourseName(String keyword, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public void assignStudentToCourse(Long courseId, Long studentId) {
+
+    }
+
+    @Override
+    public Page<CourseDTO> fetchCoursesForStudent(Long studentId, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public Page<CourseDTO> fetchNonEnrolledInCoursesForStudent(Long studentId, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public void removeCourse(Long courseId) {
+
+    }
+
+    @Override
+    public Page<CourseDTO> fetchCoursesForInstructor(Long instructorId, int page, int size) {
+        return null;
+    }
+}
