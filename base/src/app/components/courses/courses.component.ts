7d78587389b91288732158e91e2bd3b6c7d51ec3
guehara-code <gil_uehara@yahoo.com.br>
2024-05-24 17:05:25 -0300

    more studies

---
+++ base/src/app/components/courses/courses.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+
+export class CoursesComponent implements OnInit {
+
+  searchFormGroup!: FormGroup;
+
+  constructor(private modalService: NgbModal, private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control('')
+    })
+  }
+
+
+  getModal(content: any) {
+    this.modalService.open(content, {size: 'xl'})
+    console.log("Hello world")
+  }
+
+
+}
