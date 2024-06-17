e76099f14e5a523602ac4d38b5c08ae9e59c9cf1
guehara-code <gil_uehara@yahoo.com.br>
2024-06-17 08:46:49 -0300

    more studies

--- admin/src/main/java/com/javacorner/admin/AdminApplication.java
+++ admin/src/main/java/com/javacorner/admin/AdminApplication.java
@@ -7,10 +7,13 @@ package com.javacorner.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class AdminApplication {
 
 //	@Autowired
--- admin/src/main/java/com/javacorner/admin/web/CourseRestController.java
+++ admin/src/main/java/com/javacorner/admin/web/CourseRestController.java
@@ -3,6 +3,7 @@ package com.javacorner.admin.web;
 import com.javacorner.admin.dto.CourseDTO;
 import com.javacorner.admin.service.CourseService;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +18,7 @@ public class CourseRestController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('Admin')")
     public Page<CourseDTO> searchCourses(@RequestParam(name = "keyword", defaultValue = "") String keyword,
                                          @RequestParam(name = "page", defaultValue = "0") int page,
                                          @RequestParam(name = "size", defaultValue = "5") int size) {
@@ -26,18 +28,21 @@ public class CourseRestController {
     }
 
     @DeleteMapping("/{courseId}")
+    @PreAuthorize("hasAuthority('Admin')")
     public void deleteCourse(@PathVariable Long courseId) {
 
         courseService.removeCourse(courseId);
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('Admin', 'Instructor')")
     public CourseDTO saveCourse(@RequestBody CourseDTO courseDTO) {
 
         return courseService.createCourse(courseDTO);
     }
 
     @PutMapping("/{courseId}")
+    @PreAuthorize("hasAnyAuthority('Admin', 'Instructor')")
     public CourseDTO updateCourse(@RequestBody CourseDTO courseDTO, @PathVariable Long courseId) {
 
         courseDTO.setCourseId(courseId);
@@ -45,6 +50,7 @@ public class CourseRestController {
     }
 
     @PostMapping("/{courseId}/enroll/students/{studentId}")
+    @PreAuthorize("hasAuthority('Student')")
     public void enrollStudentInCourse(@PathVariable Long courseId, @PathVariable Long studentId) {
 
         courseService.assignStudentToCourse(courseId, studentId);
