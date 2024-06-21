de778c27c8eceed430051b54f63f9b701f2f54ec
guehara-code <gil_uehara@yahoo.com.br>
2024-06-21 17:04:01 -0300

    more studies

--- base/src/app/components/header/header.component.ts
+++ base/src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoggedUser } from 'src/app/model/logged-user.model';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userSub!: Subscription;
+  isAuthenticated = false;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(LoggedUser => {
+      this.isAuthenticated = !!LoggedUser;
+    })
+  }
+
+  logout() {
+    throw new Error('Method not implemented.');
   }
 
 }
