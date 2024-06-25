b4054648f77f8094c4868e3622409258c5b1c5d8
guehara-code <gil_uehara@yahoo.com.br>
2024-06-25 09:04:40 -0300

    more studies

--- base/src/app/components/navbar/navbar.component.ts
+++ base/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoggedUser } from 'src/app/model/logged-user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  userSub!: Subscription;
+  isAuthenticated = false;
+  isAdmin = false;
+  isInstructor = false;
+  isStudent = false;
+
+  instructorId: number | undefined;
+  studentId: number | undefined;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(data => {
+      this.isAuthenticated = !!data;
+      if(!this.isAuthenticated) {
+        this.initializeState();
+      }
+      this.setRole(data);
+    })
   }
 
+  setRole(loggedUser: LoggedUser | null) {
+    if (loggedUser?.roles.includes("Admin")) this.isAdmin = true;
+    else if (!!loggedUser?.instructor) {
+      this.isInstructor = true;
+      this.instructorId = loggedUser.instructor?.instructorId;
+    }
+    else if(!!loggedUser?.student) {
+      this.isStudent = true;
+      this.studentId = loggedUser.student?.studentId;
+    }
+  }
+
+  initializeState() {
+    this.isAdmin = false;
+    this.isInstructor = false;
+    this.isStudent = false;
+  }
 }
